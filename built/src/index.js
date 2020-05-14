78a960f440e78b5f8516d69b3d46c7109ce84e6f
Ervan Nur Adhitiya <ervan.nur@gmail.com>
2020-05-14 11:58:57 +0700

    feat: remove compiled files

--- built/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var draggable_grid_1 = require("./draggable-grid");
-exports.DraggableGrid = draggable_grid_1.DraggableGrid;
-exports.default = draggable_grid_1.DraggableGrid;
-//# sourceMappingURL=index.js.map
